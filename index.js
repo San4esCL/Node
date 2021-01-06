3e8d5d3d6bfd1c2cad46f6fec90b1a790322da11
unknown <san4esyt@gmail.com>
2021-01-06 12:18:58 -0300

    MiddleWARE

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3030;
 const colors = require('colors');
-app.use(express.json());
 
+//MIDDLEWARE
+function logger(req, res, next){
+    console.log(`Route Recived: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+app.use(logger);
+app.use(express.json());
+//Levantar servidor
 app.listen(port, function(){
     console.log(`Server ON http://localhost:${port}`.rainbow)
+})
+
+/*
+>>> RUTAS <<<
+*/
+app.all('/user', (req, res, next) =>{
+    console.log('Por aqui pasó');
+    next();
 })
 
 app.get('/', function(req, res){
