9778962c0b5a5ca2ab9e6c610889495c56e14916
san4escl <san4esyt@gmail.com>
2021-01-05 23:28:14 -0300

    subida

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const colors = require('colors');
+
+
+app.get('/', function(req, res){
+    res.send('<h1>Hola Mundo!</h1>');
+})
+
+app.listen(port, function(){
+    console.log(`Server ON http://localhost:${port}`.rainbow)
+})
+
